34f995e5fc2e3d3583a37f66a736b878698e95a3
CaioDetz <caiodetz654@gmail.com>
2023-05-24 12:38:17 +0000

    UP

--- 03/main.py
+++ 03/main.py
@@ -13,101 +13,99 @@ print("""
 
 
 def dimensoesObjeto():
-    try:
-        comprimento = float(input("Digite o comprimento do objeto (em cm): "))
-        largura = float(input("Digite a largura do objeto (em cm): "))
-        altura = float(input("Digite a altura do objeto (em cm): "))
-        volume = altura * largura * comprimento
-        print(f"Volume do objeto é (em cm³): {volume}")
+    while True:
+        try:
+            comprimento = float(
+                input("Digite o comprimento do objeto (em cm): "))
+            largura = float(input("Digite a largura do objeto (em cm): "))
+            altura = float(input("Digite a altura do objeto (em cm): "))
+            volume = altura * largura * comprimento
+            print(f"Volume do objeto é (em cm³): {volume}")
 
-        if volume < 1000:
-            valor = int(10)
-            return valor
-        elif volume >= 1000 and volume < 10000:
-            valor = int(20)
-            return valor
-        elif volume >= 10000 and volume < 30000:
-            valor = int(30)
-            return valor
-        elif volume >= 30000 and volume < 100000:
-            valor = int(50)
-            return valor
-        elif volume >= 100000:
-            raise TypeError("Valor Muito Alto")
-    except ValueError:
-        print("Você digitou alguma dimensão do objeto em valor não numerico")
-        print("Por favor entre com as dimensões desejadas novamente")
-        dimensoesObjeto()
-    except TypeError:
-        print("Não aceitamos objetos com as dimenções tão grandes")
-        print("Por favor entre com as dimensões desejadas novamente")
-        dimensoesObjeto()
+            if volume < 1000:
+                valor = int(10)
+                return valor
+            elif volume >= 1000 and volume < 10000:
+                valor = int(20)
+                return valor
+            elif volume >= 10000 and volume < 30000:
+                valor = int(30)
+                return valor
+            elif volume >= 30000 and volume < 100000:
+                valor = int(50)
+                return valor
+            elif volume >= 100000:
+                raise TypeError("Valor Muito Alto")
+        except ValueError:
+            print("Você digitou alguma dimensão do objeto em valor não numerico")
+            print("Por favor entre com as dimensões desejadas novamente")
+            continue
+        except TypeError:
+            print("Não aceitamos objetos com as dimenções tão grandes")
+            print("Por favor entre com as dimensões desejadas novamente")
+            continue
 
 
 def pesoObjeto():
-    try:
-        peso = float(input("Digite o peso do objeto (em kg): "))
-        if peso < 0.1:
-            multiplicador = float(1.0)
-            return multiplicador
-        elif peso >= 0.1 and peso < 1:
-            multiplicador = float(1.5)
-            return multiplicador
-        elif peso >= 1 and peso < 10:
-            multiplicador = float(2.0)
-            return multiplicador
-        elif peso >= 10 and peso < 30:
-            multiplicador = float(3.0)
-            return multiplicador
-        elif peso >= 30:
-            raise TypeError("Valor Muito Alto")
-    except ValueError:
-        print("Você digitou o peso do objeto em valor não numerico")
-        print("Por favor entre com as dimensões desejadas novamente")
-        pesoObjeto()
-    except TypeError:
-        print("Não aceitamos objetos tão pesados")
-        print("Por favor entre com as dimensões desejadas novamente")
-        pesoObjeto()
+    while True:
+        try:
+            peso = float(input("Digite o peso do objeto (em kg): "))
+            if peso < 0.1:
+                multiplicador = float(1.0)
+                return multiplicador
+            elif peso >= 0.1 and peso < 1:
+                multiplicador = float(1.5)
+                return multiplicador
+            elif peso >= 1 and peso < 10:
+                multiplicador = float(2.0)
+                return multiplicador
+            elif peso >= 10 and peso < 30:
+                multiplicador = float(3.0)
+                return multiplicador
+            elif peso >= 30:
+                raise TypeError("Valor Muito Alto")
+        except ValueError:
+            print("Você digitou o peso do objeto em valor não numerico")
+            print("Por favor entre com as dimensões desejadas novamente")
+            continue
+        except TypeError:
+            print("Não aceitamos objetos tão pesados")
+            print("Por favor entre com as dimensões desejadas novamente")
+            continue
 
 
 def rotaObjeto():
-    print("Selecione a rota:")
-    print("""RS - De Rio de Janeiro até São Paulo\nSR - De São Paulo até Rio de Janeiro\nBS - De Brasília até São Paulo\nSB - De São Paulo até Brasília\nBR - De Brasília até Rio de Janeiro\nRB - Rio de Janeiro até Brasília\n""")
-    sigla = str(input(">> ")).upper()
-    if sigla == "RS":
-        multiplicador = float(1)
-        return multiplicador
-    elif sigla == "SR":
-        multiplicador = float(1)
-        return multiplicador
-    elif sigla == "BS":
-        multiplicador = float(1.2)
-        return multiplicador
-    elif sigla == "SB":
-        multiplicador = float(1.2)
-        return multiplicador
-    elif sigla == "BR":
-        multiplicador = float(1.5)
-        return multiplicador
-    elif sigla == "RB":
-        multiplicador = float(1.5)
-        return multiplicador
-    else:
-        print("Você digitou uma rota que não existe")
-        rotaObjeto()
+    while True:
+        print("Selecione a rota:")
+        print("""RS - De Rio de Janeiro até São Paulo\nSR - De São Paulo até Rio de Janeiro\nBS - De Brasília até São Paulo\nSB - De São Paulo até Brasília\nBR - De Brasília até Rio de Janeiro\nRB - Rio de Janeiro até Brasília\n""")
+        sigla = str(input(">> ")).upper()
+        if sigla == "RS":
+            multiplicador = float(1)
+            return multiplicador
+        elif sigla == "SR":
+            multiplicador = float(1)
+            return multiplicador
+        elif sigla == "BS":
+            multiplicador = float(1.2)
+            return multiplicador
+        elif sigla == "SB":
+            multiplicador = float(1.2)
+            return multiplicador
+        elif sigla == "BR":
+            multiplicador = float(1.5)
+            return multiplicador
+        elif sigla == "RB":
+            multiplicador = float(1.5)
+            return multiplicador
+        else:
+            print("Você digitou uma rota que não existe")
+            continue
 
 
 # Chama As Funcções e pega os valores
 valor = dimensoesObjeto()
 multiplicadorPeso = pesoObjeto()
 multiplicadorRota = rotaObjeto()
-# DEBUG
-
-print(valor)
-print(multiplicadorPeso)
-print(multiplicadorRota)
-
 
 # Caucula o Total
 total = (valor * multiplicadorPeso) * multiplicadorRota
--- 04/main.py
+++ 04/main.py
@@ -10,3 +10,69 @@ print("""
 # Bem Vindo ao Controle de Estoque da bicicletaria do Caio Detz
 # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 """)
+
+# Database
+db = []
+# Contador para gerar os id
+counter = 0
+
+
+def cadastrarPeca(counter=counter, db=db):
+    # Aumenta o Contador em 1
+    counter += 1
+    # mostra algumas informações
+    print("Você Selecionou a Opção Cadastar Peça")
+    print(f"Codigo da Peça: {counter:0>3}")
+    # Gera o id Unico
+    id = f"{counter:0>3}"
+    # pega os inputs
+    nome = str(input("Por favor entre com o NOME Da Peça: "))
+    fabricante = str(input("Por favor entre com o FABRICANTE Da Peça: "))
+    valor = str(input("Por favor entre com o VALOR(R$) Da Peça: "))
+    # Adiciona no banco de dados
+    db.append({
+        "id": id,
+        "nome": nome,
+        "fabricante": fabricante,
+        "valor": valor
+    })
+
+
+def consultarPeca(counter=counter, db=db):
+
+    pass
+
+
+def removerPeca(counter=counter, db=db):
+    # pega o id por inputo do usuario
+    id = str(input("Digite o codigo da peça a ser removida: "))
+    print(db)
+    for i in db:
+        # verifica se o id é o mesmo do objeto
+        if id == i["id"]:
+            # Pega o Index do id a ser removido
+            uid = db.index(i)
+            # remove o id
+            del db[uid]
+            # para o loop
+
+            print(db)
+            break
+        else:
+            continue
+
+
+while True:
+    print("Escolha a opção desejada:\n1-Cadastrar Peças\n2-Consultar Peças\n3-Remover Peças\n4-Sair")
+    op = str(input(">> "))
+    if op == "1":
+        cadastrarPeca()
+    elif op == "2":
+        consultarPeca()
+    elif op == "3":
+        removerPeca()
+    elif op == "4":
+        break
+    else:
+        print("Opção Invalida")
+        continue
